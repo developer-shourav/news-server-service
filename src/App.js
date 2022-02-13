efc2005aed3164e04db2097dec238a906eacf1e2
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-02-13 23:05:33 +0600

    row bootstrap testing insted of react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
-import { Card, Col, Row } from "react-bootstrap";
-import Newslist from "./components/Newslist/Newslist";
 
 function App() {
   const [news, setNews] = useState({});
   useEffect(() => {
     fetch(
-      `https://newsapi.org/v2/everything?q=Apple&from=2022-02-12&sortBy=popularity&apiKey=394b3210ba7544569f3f15fbc9d039b9`
-    )
+      `https://newsapi.org/v2/everything?q=Apple&from=2022-02-12&sortBy=popularity&apiKey=394b3210ba7544569f3f15fbc9d039b9`)
       .then((res) => res.json())
       .then((data) => setNews(data.articles));
   }, []);
 
+  console.log(news);
   return (
     <div>
-    
-      <Row xs={1} md={3} className="g-4">
-       {
-        news.map(nw =>  <Newslist news ={nw} > </Newslist>) 
-       }
-      </Row>
+   <div class="row row-cols-1 row-cols-md-3 g-4">
+  
+  
+
+
+</div>
     </div>
   );
 }
---
+++ src/components/News/News.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const News = () => {
+  return (
+    <div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="..." class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default News;
